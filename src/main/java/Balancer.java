3e436ae3ead7bdb523b8b6e9d59ecec48ba8c417
Dmitry Parshin <parshin.galaxy2@gmail.com>
2019-05-10 10:34:47 +0300

    Update Balancer.java

--- src/main/java/Balancer.java
+++ src/main/java/Balancer.java
@@ -1,2 +1,37 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Balancer {
+
+    private final Queue<StatedContinuation> taskQueue;
+
+    public Balancer() {
+        taskQueue = new PriorityQueue<>(new ContinuationStateComparator());
+    }
+
+    public void addTask(StatedContinuation task) {
+        taskQueue.add(task);
+    }
+
+    public void addTasks(StatedContinuation... tasks) {
+        taskQueue.addAll(Arrays.asList(tasks));
+    }
+
+    public void executeTasks() {
+        if (!taskQueue.isEmpty()) {
+            StatedContinuation task = taskQueue.poll();
+            Thread t = new Thread(task::run);
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (!task.isDone()) {
+                taskQueue.add(task);
+            }
+            executeTasks();
+        }
+    }
 }
